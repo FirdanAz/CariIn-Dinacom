1bd4eca00418ee0fb369195da9b6275518925c58
JavierGavra <javiergavra01@gmail.com>
2023-01-05 00:15:54 +0700

    style: [JG] adding Custom AppBar on homePage

--- lib/common/color_app.dart
+++ lib/common/color_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ColorApp {
-  static const Color primaryColor = Color(0xffffffff);
-  static const Color secondaryColor = Color(0xffffffff);
-  static const Color accentColor = Color(0xffffffff);
+  static const Color primaryColor = Color(0xff303841);
+  static const Color secondaryColor = Color(0xff3A4750);
+  static const Color accentColor = Color(0xff00ADB5);
 }
---
+++ lib/ui/widget/home_header.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lowongan_pekerjaan/common/color_app.dart';
+
+class HomeHeader extends SliverPersistentHeaderDelegate {
+  double maxHeight = 87;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      height: maxHeight,
+      padding: const EdgeInsets.only(top: 15, left: 25, right: 25, bottom: 5),
+      decoration: const BoxDecoration(
+        color: ColorApp.primaryColor,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x40000000),
+            offset: Offset(0, 4),
+            blurRadius: 4,
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 40,
+            width: 310.w,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+          ),
+          const Spacer(),
+          Container(
+            height: 2,
+            width: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(1),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => maxHeight;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => 70;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      false;
+}
