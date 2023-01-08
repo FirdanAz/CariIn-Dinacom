c84d01b4aa778e27b329ea751587d3dc792bfbf7
JavierGavra <javiergavra01@gmail.com>
2023-01-08 22:08:57 +0700

    style: [JG] added UI for Wishlist

---
+++ lib/ui/wishlist/wishlist.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lowongan_pekerjaan/common/color_app.dart';
+import 'package:lowongan_pekerjaan/ui/widget/lowongan_card_horizontal.dart';
+
+class WishlistPage extends StatelessWidget {
+  const WishlistPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Wishlist",
+          style: TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w500),
+        ),
+        titleSpacing: 10,
+        toolbarHeight: 70.h,
+        backgroundColor: ColorApp.secondaryColor,
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        padding: EdgeInsets.symmetric(vertical: 19.h, horizontal: 10.w),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 6.h),
+            child: LowonganCardHorizontal(),
+          );
+        },
+      ),
+    );
+  }
+}
