d4fc80626ea6d03bf662890329269523d2fd3ff1
JavierGavra <javiergavra01@gmail.com>
2023-01-16 23:55:26 +0700

    style+feat: [JG] added search history

--- lib/ui/search/before_search.dart
+++ lib/ui/search/before_search.dart
@@ -1,120 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'package:lowongan_pekerjaan/common/color_app.dart';
+// import 'package:lowongan_pekerjaan/common/color_app.dart';
 
-class BeforeSearch extends StatelessWidget {
-  const BeforeSearch({super.key});
+// class BeforeSearch extends StatelessWidget {
+//   const BeforeSearch({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    // final screenSize = MediaQuery.of(context).size;
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Riwayat",
-                  style: TextStyle(
-                      fontSize: 13.sp,
-                      fontWeight: FontWeight.w500,
-                      color: const Color(0x803A4750)),
-                ),
-                SizedBox(height: 10.h),
-                for (int i = 0; i < 3; i++)
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 10.h),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.history,
-                          size: 20.w,
-                          color: ColorApp.primaryColor,
-                        ),
-                        SizedBox(width: 10.w),
-                        Text(
-                          "flutter development",
-                          style: TextStyle(
-                            fontSize: 13.sp,
-                            fontWeight: FontWeight.w500,
-                            color: const Color(0x803A4750),
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-              ],
-            ),
-            SizedBox(height: 10.h),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Kategori",
-                  style: TextStyle(
-                      fontSize: 13.sp,
-                      fontWeight: FontWeight.w500,
-                      color: const Color(0x803A4750)),
-                ),
-                SizedBox(height: 10.h),
-                Wrap(
-                  spacing: 10.w,
-                  runSpacing: 10.h,
-                  children: [
-                    _categoryButton("Akutansi dan Keuangan"),
-                    _categoryButton("Customer Service"),
-                    _categoryButton("Penjualan"),
-                    _categoryButton("Logistik dan Kurir"),
-                    _categoryButton("Pemasaran"),
-                    _categoryButton("kesehatan, Salon, Kecantikan"),
-                    _categoryButton("It dan Teknisi"),
-                    _categoryButton("Tenaga Administrasi"),
-                    _categoryButton("OB, Kebersihan dan Keamanan"),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     // final screenSize = MediaQuery.of(context).size;
+//     return ;
+//   }
 
-  Widget _categoryButton(String text) {
-    return ActionChip(
-      padding: EdgeInsets.symmetric(horizontal: 17.w, vertical: 11.h),
-      backgroundColor: ColorApp.accentColor,
-      labelPadding: EdgeInsets.zero,
-      onPressed: () {},
-      elevation: 10,
-      pressElevation: 16,
-      shadowColor: const Color(0x80AAAACC),
-      label: Text(
-        text,
-        style: TextStyle(
-            fontSize: 12.sp, fontWeight: FontWeight.w500, color: Colors.white),
-      ),
-    );
-    // return Container(
-    //   height: 40,
-    //   width: 100,
-    //   alignment: Alignment.center,
-    //   padding: const EdgeInsets.symmetric(horizontal: 17),
-    //   decoration: BoxDecoration(
-    //     color: accentColor,
-    //     borderRadius: BorderRadius.circular(15),
-    //   ),
-    //   child: MyText(
-    //     text: text,
-    //     size: 12,
-    //     weight: FontWeight.w500,
-    //     color: primaryColor,
-    //   ),
-    // );
-  }
-}
+  
+//   }
+// }
--- lib/ui/search/search.dart
+++ lib/ui/search/search.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lowongan_pekerjaan/common/color_app.dart';
+import 'package:lowongan_pekerjaan/database/history_search_database.dart';
 import 'package:lowongan_pekerjaan/ui/search/before_search.dart';
 import 'package:lowongan_pekerjaan/ui/search/result.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sqflite/sqflite.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -14,8 +16,11 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   ScrollController? _scrollController;
   final TextEditingController _searchTextController = TextEditingController();
+  List<Map<String, dynamic>>? _allDataHistorySearchDatabase;
   final double _height = 149;
   bool _lastStatus = true;
+  bool isDatabaseLoading = true;
+  bool isRiwayatEmpty = true;
 
   bool get _isShrink {
     return _scrollController != null &&
@@ -31,10 +36,63 @@ class _SearchPageState extends State<SearchPage> {
     }
   }
 
+  void getData() async {
+    _allDataHistorySearchDatabase =
+        await HistorySearchDatabase.instance.readAll();
+    print(_allDataHistorySearchDatabase.toString());
+    setState(() {
+      isDatabaseLoading = false;
+      isRiwayatEmpty = _allDataHistorySearchDatabase!.isEmpty;
+    });
+  }
+
+  void deleteHistory(var context) async {
+    for (var i = 1;
+        i <=
+            _allDataHistorySearchDatabase![
+                _allDataHistorySearchDatabase!.length - 1]["_id"];
+        i++) {
+      await HistorySearchDatabase.instance.delete(i);
+    }
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+            'Ingin Menghapus Riwayat Pencarian?',
+            style: TextStyle(color: Colors.black, fontSize: 15),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel',
+                  style: TextStyle(color: ColorApp.primaryColor)),
+            ),
+            TextButton(
+              onPressed: () {
+                deleteHistory(context);
+                Navigator.pop(context);
+                setState(() {
+                  isRiwayatEmpty = true;
+                });
+              },
+              child: const Text('OK', style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    getData();
     _scrollController = ScrollController()..addListener(_scrollListener);
   }
 
@@ -125,8 +183,12 @@ class _SearchPageState extends State<SearchPage> {
                               fontSize: 14.sp,
                               color: ColorApp.secondaryColor.withOpacity(0.5)),
                         ),
-                        onEditingComplete: () {
+                        onEditingComplete: () async {
                           if (_searchTextController.text.isNotEmpty) {
+                            await HistorySearchDatabase.instance.create({
+                              HistorySearchDatabase.columnText:
+                                  _searchTextController.text
+                            });
                             Navigator.pushReplacement(
                               context,
                               MaterialPageRoute(
@@ -144,9 +206,158 @@ class _SearchPageState extends State<SearchPage> {
               ),
             ),
           ),
-          const BeforeSearch(),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  isDatabaseLoading
+                      ? const Center(child: CircularProgressIndicator())
+                      : isRiwayatEmpty
+                          ? const SizedBox()
+                          : Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Riwayat",
+                                      style: TextStyle(
+                                          fontSize: 13.sp,
+                                          fontWeight: FontWeight.w500,
+                                          color: const Color(0x803A4750)),
+                                    ),
+                                    InkWell(
+                                      onTap: () {
+                                        _showMyDialog();
+                                      },
+                                      child: Text(
+                                        "Hapus Riwayat",
+                                        style: TextStyle(
+                                            fontSize: 13.sp,
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.red),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 10.h),
+                                ListView.separated(
+                                  shrinkWrap: true,
+                                  padding: EdgeInsets.zero,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  itemCount:
+                                      _allDataHistorySearchDatabase!.length < 3
+                                          ? _allDataHistorySearchDatabase!
+                                              .length
+                                          : 3,
+                                  separatorBuilder: (context, index) => Divider(
+                                    height: 10.h,
+                                    color: Colors.transparent,
+                                  ),
+                                  itemBuilder: (context, index) {
+                                    int reverseIndex =
+                                        _allDataHistorySearchDatabase!.length -
+                                            1 -
+                                            index;
+                                    return Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.history,
+                                          size: 20.w,
+                                          color: ColorApp.primaryColor,
+                                        ),
+                                        SizedBox(width: 10.w),
+                                        Text(
+                                          _allDataHistorySearchDatabase![
+                                                  reverseIndex]["text"]
+                                              .toString(),
+                                          style: TextStyle(
+                                            fontSize: 13.sp,
+                                            fontWeight: FontWeight.w500,
+                                            color: const Color(0x803A4750),
+                                          ),
+                                        )
+                                      ],
+                                    );
+                                  },
+                                ),
+                                SizedBox(height: 20.h),
+                              ],
+                            ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Kategori",
+                        style: TextStyle(
+                            fontSize: 13.sp,
+                            fontWeight: FontWeight.w500,
+                            color: const Color(0x803A4750)),
+                      ),
+                      SizedBox(height: 10.h),
+                      Wrap(
+                        spacing: 10.w,
+                        runSpacing: 10.h,
+                        children: [
+                          _categoryButton("Akutansi dan Keuangan"),
+                          _categoryButton("Customer Service"),
+                          _categoryButton("Penjualan"),
+                          _categoryButton("Logistik dan Kurir"),
+                          _categoryButton("Pemasaran"),
+                          _categoryButton("kesehatan, Salon, Kecantikan"),
+                          _categoryButton("It dan Teknisi"),
+                          _categoryButton("Tenaga Administrasi"),
+                          _categoryButton("OB, Kebersihan dan Keamanan"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
+  }
+
+  Widget _categoryButton(String text) {
+    return ActionChip(
+      padding: EdgeInsets.symmetric(horizontal: 17.w, vertical: 11.h),
+      backgroundColor: ColorApp.accentColor,
+      labelPadding: EdgeInsets.zero,
+      onPressed: () {},
+      elevation: 10,
+      pressElevation: 16,
+      shadowColor: const Color(0x80AAAACC),
+      label: Text(
+        text,
+        style: TextStyle(
+            fontSize: 12.sp, fontWeight: FontWeight.w500, color: Colors.white),
+      ),
+    );
+    // return Container(
+    //   height: 40,
+    //   width: 100,
+    //   alignment: Alignment.center,
+    //   padding: const EdgeInsets.symmetric(horizontal: 17),
+    //   decoration: BoxDecoration(
+    //     color: accentColor,
+    //     borderRadius: BorderRadius.circular(15),
+    //   ),
+    //   child: MyText(
+    //     text: text,
+    //     size: 12,
+    //     weight: FontWeight.w500,
+    //     color: primaryColor,
+    //   ),
+    // );
   }
 }
